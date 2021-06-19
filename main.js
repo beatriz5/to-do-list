4b038add3462bd4a57c0dfd8ee70753195884261
seaweed63 <fortnite63@protonmail.com>
2021-06-18 23:37:03 -0400

    memory

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+//let ul = document.getElementById("task-list");
+//console.log(ul);
+//localStorage.setItem('myCat', 'Tom');
+//localStorage.clear();
+let task1 = {
+    marcado: false,
+    texto: 'Buy milk',
+};
+
+let task2 = {
+    marcado: true,
+    texto: 'Get a cow',
+};
+
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+let array = localStorage.getItem('taskList');
+let cow = JSON.parse(array);
+console.log("Hola");
+console.log(cow);
+
+
+
+
 //ADD NEW TASK
 document.getElementById("add-task-button").addEventListener('click', function () {
     let boxValue = document.getElementById('input-task').value;
@@ -28,6 +52,18 @@ document.getElementById("add-task-button").addEventListener('click', function ()
 
     document.getElementById("input-task").value = '';
 
+    //Add task
+    let task3 = {
+        marcado: false,
+        texto: `${boxValue}`
+    };
+    taskList.push(task1);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    let array = localStorage.getItem('taskList');
+    let cow = JSON.parse(array);
+    console.log("mutable");
+    console.log(cow);
+
     input.addEventListener('change', e => {
         if (e.target.checked) {
             span.classList.add("task-done");
@@ -42,6 +78,9 @@ let btn = document.getElementsByClassName('delete-btn');
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function (e) {
         e.currentTarget.parentNode.remove();
+        const cat = localStorage.getItem('my');
+        console.log(cat);
+
     }, false);
 }
 
@@ -51,10 +90,10 @@ let checkbox = document.querySelectorAll("input[type=checkbox]");
 for (let i = 0; i < checkbox.length; i++) {
     checkbox[i].addEventListener('change', function (e) {
         let k = checkbox[i].nextElementSibling;
-        if(e.target.checked){
+        if (e.target.checked) {
             k.classList.add("task-done");
-        }else{
-            if (k.classList.contains('task-done')){
+        } else {
+            if (k.classList.contains('task-done')) {
                 k.classList.remove("task-done");
             }
         }
